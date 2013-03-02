fdba510ed6c55b6554ff9b942b2cbe22969a2144
cedricporter <cedricporter@be201379-6a98-8680-66f9-b1c729131440>
2013-03-02 02:20:42 +0000

    Hello world!
    git-svn-id: https://cedricporter-yagra.googlecode.com/svn/trunk@6 be201379-6a98-8680-66f9-b1c729131440

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Hua Liang[Stupid ET] <et@everet.org>
+#
+print "Content-type:text/html\r\n\r\n"
+print ''
+print ''
+print 'Hello ET'
+print ''
+print ''
