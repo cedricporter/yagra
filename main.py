4acceb722911391db5ff51c636ec9d362ac34529
cedricporter <cedricporter@be201379-6a98-8680-66f9-b1c729131440>
2013-03-02 02:20:50 +0000

    print cgi environment
    git-svn-id: https://cedricporter-yagra.googlecode.com/svn/trunk@7 be201379-6a98-8680-66f9-b1c729131440

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 # Author: Hua Liang[Stupid ET] <et@everet.org>
 #
-print "Content-type:text/html\r\n\r\n"
-print ''
-print ''
-print 'Hello ET'
-print ''
-print ''
+
+import cgitb
+cgitb.enable()
+
+import cgi
+
+print "Content-Type: text/html"
+print
+
+cgi.print_environ()
