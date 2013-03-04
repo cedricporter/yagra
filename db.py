450b4467f4f2b9076cf737edd293359fbd7c2d79
cedricporter <cedricporter@be201379-6a98-8680-66f9-b1c729131440>
2013-03-04 12:09:09 +0000

    开始连接数据库，增加了db.py...
    git-svn-id: https://cedricporter-yagra.googlecode.com/svn/trunk@16 be201379-6a98-8680-66f9-b1c729131440

---
+++ db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Hua Liang[Stupid ET] <et@everet.org>
+#
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",
+                     user="yagra",
+                     passwd="yagra_p@$$w0rd",
+                     db="yagra")
+
+
+def random_string(length=10):
+    import random
+    import string
+
+    return "".join(random.choice(string.letters) for i in xrange(length))
+
+
+def main():
+    import pprint
+    import time
+
+    c = db.cursor()
+    c.execute("""
+    INSERT INTO `yagra`.`yagra_user`
+    (
+    `user_login`,
+    `user_pass`,
+    `user_nicename`,
+    `user_email`,
+    `user_register`,
+    `user_status`)
+    VALUES
+    (%s, %s, %s, %s, %s, %s)
+    """, (random_string(), "p@$$w0rd", "Cedric Porter",
+          random_string() + "@gmail.com",
+           time.strftime('%Y-%m-%d %H:%M:%S'), str(1)))
+    db.commit()
+    c.execute("SELECT * FROM yagra_user")
+    pprint.pprint(c.fetchall())
+
+    c.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
