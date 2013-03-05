1f7046d06c19f67ecf5fc4bde96bf8d6e21af19d
cedricporter <cedricporter@be201379-6a98-8680-66f9-b1c729131440>
2013-03-05 07:18:12 +0000

    增加建表和用户的sql文件，请用root权限source一下它。
    git-svn-id: https://cedricporter-yagra.googlecode.com/svn/trunk@20 be201379-6a98-8680-66f9-b1c729131440

---
+++ scheme.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS `yagra_image`;
+DROP TABLE IF EXISTS `yagra_user`;
+
+-- 用户表
+CREATE TABLE `yagra_user` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_login` varchar(60) NOT NULL DEFAULT '',
+  `user_pass` varchar(64) NOT NULL DEFAULT '',
+  `user_nicename` varchar(50) NOT NULL DEFAULT '',
+  `user_email` varchar(100) NOT NULL DEFAULT '',
+  `user_register` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `user_login_key` (`user_login`),
+  KEY `user_email_key` (`user_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 图片表
+CREATE TABLE `yagra_image` (
+  `image_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `filename` varchar(256) NOT NULL,
+  `upload_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`image_id`),
+  KEY `user_id_ind` (`user_id`),
+  CONSTRAINT `yagra_image_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `yagra_user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 添加用户
+GRANT select, update, insert ON yagra.* to `yagra`@`localhost` IDENTIFIED BY 'yagra_p@$$w0rd';
+FLUSH PRIVILEGES;
