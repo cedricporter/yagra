2580833d3bdbd6ad8b70a79caf0bfd28cbbd53be
cedricporter <cedricporter@be201379-6a98-8680-66f9-b1c729131440>
2013-03-06 07:50:19 +0000

    基本完成了session类，花了2个小时，超过计划一个小时.
    git-svn-id: https://cedricporter-yagra.googlecode.com/svn/trunk@23 be201379-6a98-8680-66f9-b1c729131440

--- session.py
+++ session.py
@@ -5,23 +5,41 @@
 
 import pickle
 import base64
+import time
+import datetime
+import uuid
 
 
 class Session(object):
-    def __init__(self):
+    def __init__(self, store):
         self._data = {}
+        self.store = store
 
     def __contains__(self, name):
         return name in self._data
 
-    def __getattr__(self, name):
+    def __getitem__(self, name):
         return self._data[name]
 
-    def __setattr__(self, name, value):
+    def __setitem__(self, name, value):
         self._data[name] = value
 
-    def __delattr__(self, name):
+    def __delitem__(self, name):
         del self._data[name]
+
+    def _load(self, session_id=None):
+        self.session_id = session_id
+        if not self.session_id:
+            self.session_id = self._generate_session_id()
+        self._data = self.store[self.session_id] or dict()
+
+    def _save(self):
+        self.store[self.session_id] = self._data
+
+    def _generate_session_id(self):
+        "Generate a 128 bytes string"
+        session_id = "".join(uuid.uuid4().hex for i in xrange(4))
+        return session_id
 
 
 class Store(object):
@@ -42,8 +60,73 @@ class Store(object):
         pickled = base64.decodestring(session_data)
         return pickle.loads(pickled)
 
+    def cleanup(self, timeout):
+        raise NotImplementedError
+
 
 class MySQLStore(Store):
     def __init__(self, db, table_name):
         self.db = db
         self.table = table_name
+
+    def __contains__(self, key):
+        c = self.db.cursor()
+        sql = "SELECT * FROM " + self.table + " WHERE session_id = %s"
+        row = c.execute(sql, (key, ))
+        c.close()
+        return bool(row)
+
+    def __getitem__(self, key):
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        c = self.db.cursor()
+        sql = "SELECT data FROM " + self.table + " WHERE session_id = %s"
+        row = c.execute(sql, (key, ))
+        if row:
+            data = c.fetchone()[0]
+            sql = "UPDATE " + self.table + " SET atime = %s WHERE session_id = %s"
+            c.execute(sql, (now, key))
+            self.db.commit()
+            c.close()
+            return self.decode(data)
+
+        c.close()
+
+    def __setitem__(self, key, value):
+        pickled = self.encode(value)
+        c = self.db.cursor()
+        if key in self:
+            sql = "UPDATE " + self.table + " SET data = %s WHERE session_id = %s"
+            c.execute(sql, (pickled, key))
+        else:
+            sql = "INSERT INTO " + self.table + " (session_id, data) VALUES (%s, %s)"
+            c.execute(sql, (key, pickled))
+        self.db.commit()
+        c.close()
+
+    def __delitem__(self, key):
+        c = self.db.cursor()
+        sql = "DELETE FROM " + self.table + " WHERE session_id = %s"
+        c.execute(sql, (key, ))
+        self.db.commit()
+        c.close()
+
+    def cleanup(self, timeout):
+        timeout = datetime.timedelta(timeout / (24.0 * 60.0 * 60.0))
+        last_allowed_time = datetime.datetime.now() - timeout
+        last_allowed_time = last_allowed_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        c = self.db.cursor()
+        sql = "DELETE FROM " + self.table + " WHERE atime < %s"
+        c.execute(sql, (last_allowed_time, ))
+        self.db.commit()
+        c.close()
+
+
+if __name__ == '__main__':
+    from db import db
+    session = Session(MySQLStore(db, "yagra_session"))
+    session._load("4310fee7dcf344e894d52f57537b9f5ccc6be19e755f48b6b5c668399c29b3e80fa7bd8942b94bc4870a629ac374341bc7cebcd3868e4f999722a4bb71df603f")
+    print session["name"], session["age"]
+    session["name"] = "Cedric Porter"
+    session["age"] = 22
+    session._save()
